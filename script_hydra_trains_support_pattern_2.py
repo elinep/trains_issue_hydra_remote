0a867a6ce61dcbafed0a5fb68dac33e6c1e71f13
pierre eline <pierre.eline@parrot.com>
2020-11-08 20:57:00 +0100

    [DEV] add scripts to test trains==0.16.3 hydra support

---
+++ script_hydra_trains_support_pattern_2.py
@@ -0,0 +1,30 @@
+import os
+from trains import Task
+import hydra
+from hydra.core.config_store import ConfigStore
+from dataclasses import dataclass
+
+
+# hydra config
+@dataclass
+class AppConfig:
+    task_name: str = os.path.basename(__file__)
+    task_project: str = "hydra_project"
+    a_parameter: int = 0
+
+
+# register config
+cs = ConfigStore.instance()
+cs.store(name="config", node=AppConfig)
+
+
+@hydra.main(config_name="config")
+def main(config: AppConfig):
+    print("config is:")
+    print(config)
+
+
+# see https://github.com/allegroai/trains/issues/219#issuecomment-723503923
+if __name__ == "__main__":
+    Task.init(AppConfig().task_project, AppConfig().task_name)
+    main()
